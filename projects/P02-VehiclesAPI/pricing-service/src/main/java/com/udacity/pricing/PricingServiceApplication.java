1883b254026e3501d62ea6026c76dba9ba77727f
Kennedy Kariuki <kennedy.kariuki@tracom.co.ke>
2019-08-08 20:53:26 +0300

    pricing controller unit test

--- projects/P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ projects/P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -2,11 +2,12 @@ package com.udacity.pricing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
- * TODO: Convert the application from a REST API to a microservice.
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PricingServiceApplication {
 
--- projects/P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/api/PricingControllerTest.java
+++ projects/P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/api/PricingControllerTest.java
@@ -1,4 +1,37 @@
 package com.udacity.pricing.api;
 
+import com.udacity.pricing.service.PricingService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PricingController.class)
 public class PricingControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    PricingService pricingService;
+
+    @Test
+    public void test_get_price() throws Exception {
+        mockMvc.perform(get("/services/price")
+                    .param("vehicleId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().json("{}"));
+
+        // verify(PricingService.getPrice(1), times(1));
+    }
 }
