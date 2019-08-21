c6a85a7cb9e7a68f33897a89fe664f0e2245c94f
Kennedy Kariuki <kennedy.kariuki@tracom.co.ke>
2019-08-21 20:00:25 +0300

    added review service

--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/services/ReviewService.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/services/ReviewService.java
@@ -1,4 +1,17 @@
 package com.udacity.course3.reviews.services;
 
+import com.udacity.course3.reviews.repository.ReviewRepository;
+import com.udacity.course3.reviews.repository.mongo.MongoReviewRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReviewService {
+
+    private MongoReviewRepository mongoReviewRepository;
+    private ReviewRepository reviewRepository;
+
+    public ReviewService(MongoReviewRepository mongoReviewRepository, ReviewRepository reviewRepository) {
+        this.mongoReviewRepository = mongoReviewRepository;
+        this.reviewRepository = reviewRepository;
+    }
 }
